b1b8029f0719f9e54f5b58a7643d32717c0239b4
Alan <alan4tomaz8@gmail.com>
2024-12-14 22:20:33 -0300

    Finish Email Send with Captcha Logic

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ dotenv.config();
 /* USE JSON REQUESTS */
 app.use(express.json());
 /* CORS RULES */
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow All Origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed Methods
+}));
+app.options('*', cors()); // Response all OPTIONS Requests
 /* GET THE DIRECTORY */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
