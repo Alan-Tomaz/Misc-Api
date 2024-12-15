3fcf0aa7d677404b6e7b96ff4f691963fe2be3f3
Alan <alan4tomaz8@gmail.com>
2024-12-14 21:46:22 -0300

    Finish Email Send with Captcha Logic

--- controllers/email.js
+++ controllers/email.js
@@ -1,3 +1,38 @@
+import nodemailer from "nodemailer";
+
 export const sendEmail = async (req, res) => {
-    res.json({ isError: false })
+    try {
+        const [name, email, subject, message] = req.body;
+
+        if ((name == null || !name) || (email == null || !email) || (subject == null || !subject) || (message == null || !message)) {
+            return res.status(500).json({ isError: true, error: 2, message: "Fill All Forms" });
+        }
+
+        /* SEND EMAIL WITH NODEMAILER */
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: process.env.MAIL_ADDRESS,
+                pass: process.env.MAIL_PASSWORD,
+                clientId: process.env.OAUTH_CLIENT_ID,
+                clientSecret: process.env.OAUTH_CLIENT_SECRET,
+                refreshToken: process.env.OAUTH_REFRESH_TOKEN
+            }
+        });
+
+        const mailOptions = {
+            from: email,
+            to: "alantomaz.dev@gmail.com",
+            subject: `Contato de ${name} - ${subject}`,
+            text: message
+        };
+
+        transporter.sendMail(mailOptions);
+
+        res.status(200).json({ isError: false });
+    } catch (error) {
+        console.error("Error to Send Email: ", error);
+        res.status(500).json({ isError: true, error: 3, message: "Internal Mail Error" });
+    }
 }
--- middlewares/checkCaptcha.js
+++ middlewares/checkCaptcha.js
@@ -7,12 +7,12 @@ export const checkCaptcha = async (req, res, next) => {
         );
 
         if (!response.data.success) {
-            return res.status(400).json({ isError: true, error: 1 });
+            return res.status(400).json({ isError: true, error: 1, message: "Invalid Captcha Return" });
         } else {
             next();
         }
     } catch (error) {
         console.log({ message: "Erro Interno do Servidor", error })
-        res.status(500).json({ isError: true, error: 0 });
+        res.status(500).json({ isError: true, error: 0, message: "Internal Captcha Check Error" });
     }
 }
